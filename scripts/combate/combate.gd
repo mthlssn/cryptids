233beb68c9868908655178cdae061555dce39663
mthlssn <thalissonribeirodossantos05@gmail.com>
2022-10-21 04:59:14 -0300

    update files

--- scripts/combate/combate.gd
+++ scripts/combate/combate.gd
@@ -1,10 +1,11 @@
 extends CanvasLayer
 
 onready var primeiro_botao := $personagens/player/botao
+var sla = load("res://data/dialogs/pt_BR/cena_3/saida_de_baixo.tres")
 
 func _ready():
 	primeiro_botao.grab_focus()
+	DialogBox.call_dialog_box(false, sla.msg_queue, null, null)
 
 func _on_botao_pressed():
 	$acoes/agir/botao.grab_focus()
-	
